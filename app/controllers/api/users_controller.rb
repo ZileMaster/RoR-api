6d3368934dfb70c877bbe3a04e2dbcc7d4f63cf3
ZileMaster <azivkovic10120ri@raf.rs>
2023-01-10 12:35:19 +0100

    adding a token in create users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,7 +15,8 @@ class Api::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user
+            token = encode_token({ user_id: @user.id })
+            render json: { user: @user, token: token}, status: 200
         else
             render error: { error: 'unable to make user'}, status: 400
         end
