06335cd48aa2e8d9a89a17dc24c198b0d3343313
ZileMaster <azivkovic10120ri@raf.rs>
2023-02-06 15:40:56 +0100

    fixing cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://www.azivkovic1389.site/"
+    origins "https://www.azivkovic1389.site"
 
     resource "*",
       headers: :any,
