a4530f9f73919e4ce74cc5eae13c87336fea91d0
ZileMaster <azivkovic10120ri@raf.rs>
2023-02-06 11:56:14 +0100

    idk even

---
+++ db/migrate/20230206105551_change_password_to_password_digest_in_users.rb
@@ -0,0 +1,5 @@
+class ChangePasswordToPasswordDigestInUsers < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-admins = Admin.create(username: "admin", password_digest: "password", email: "admin@example.com", first_name: "John", last_name: "Doe")
+admins = Admin.create(username: "az_zile01", password_digest: "password1", email: "aleksaa.zivkovic01@gmail.com", first_name: "Aleksa", last_name: "Zivkovic")
 
-5.times do |i|
-    User.create(username: "user#{i}", password_digest: "password", email: "user#{i}@example.com", first_name: "First#{i}", last_name: "Last#{i}", admin_id: admins.id)
-  end
 
+User.create(
+    username: "example_username",
+    password: "12345678",
+    email: "example_user@example.com",
+    first_name: "Example",
+    last_name: "User",
+    admin_id: 1
+  )
+  
 projectPage = ProjectPage.create(admin_id: admins.id);
 personalPage = PersonalBlog.create(admin_id: admins.id);
 travelPage = TravelBlog.create(admin_id: admins.id);
@@ -23,19 +29,19 @@ noticeBoard = NoticeBoard.create(admin_id: admins.id);
   end
 
 3.times do |i|
-    Post.create(headline:"this is the head #{i}", description: "the description of the info number #{i}", topic: "project", travel_blog_id: travelPage.id, personal_blog_id: personalPage.id, project_page_id: projectPage.id, likes: 0);
+    Post.create(headline:"this is the head #{i}", description: "the description of the info number #{i}", text: "sample text for the post, it should be very veryv very big but this is just a seed so it's lil", topic: "project", travel_blog_id: travelPage.id, personal_blog_id: personalPage.id, project_page_id: projectPage.id, likes: 0);
 end
 
 3.times do |i|
-    Post.create(headline:"this is the head #{i}", description: "the description of the info number #{i}", topic: "travel", travel_blog_id: travelPage.id, personal_blog_id: personalPage.id, project_page_id: projectPage.id, likes: 0);
+    Post.create(headline:"this is the head #{i}", description: "the description of the info number #{i}", text: "sample text for the post, it should be very veryv very big but this is just a seed so it's lil" , topic: "travel", travel_blog_id: travelPage.id, personal_blog_id: personalPage.id, project_page_id: projectPage.id, likes: 0);
 end
 
 3.times do |i|
-    Post.create(headline:"this is the head #{i}", description: "the description of the info number #{i}", topic: "personal", travel_blog_id: travelPage.id, personal_blog_id: personalPage.id, project_page_id: projectPage.id, likes: 0);
+    Post.create(headline:"this is the head #{i}", description: "the description of the info number #{i}", topic: "personal", text: "sample text for the post, it should be very veryv very big but this is just a seed so it's lil" , travel_blog_id: travelPage.id, personal_blog_id: personalPage.id, project_page_id: projectPage.id, likes: 0);
 end
 
 3.times do |i|
-    Komentar.create(headline:"this is the head #{i}", description: "the description of the info number #{i}", user_id: User.id, post_id:Post.id, rating: 0)
+    Komentar.create(headline:"this is the head #{i}", description: "the description of the info number #{i}", user_id: 1, post_id:{i}, rating: 0)
 end
 
 news = Newsletter.create(user_id: User.id);
