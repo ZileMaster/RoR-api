e65957d92365287b9c65469c094c00180d40ab55
ZileMaster <azivkovic10120ri@raf.rs>
2023-02-07 10:29:30 +0100

    adding more origins on cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://www.azivkovic1389.site"
+    origins "https://www.azivkovic1389.site", "https://azivkovic1389.site/", "http://localhost:8080/"
 
     resource "*",
       headers: :any,
